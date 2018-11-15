9565f37f8424d9e989c4b8fc1de5b92e8291280b
petertenghamn <petertenghamn@hotmail.com>
2018-11-15 20:23:02 +0100

    Final gitignore update

--- src/stud/hkr/Main.java
+++ src/stud/hkr/Main.java
@@ -3,7 +3,6 @@ package stud.hkr;
 public class Main {
 
     public static void main(String[] args) {
-        int b = 0;
-        //testing the gitignore and who knows whats happening
+	    // write your code here
     }
 }
