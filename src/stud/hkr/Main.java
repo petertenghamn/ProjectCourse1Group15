d7447765ad8d142fbbec25ce37ec646b94f36f32
Öjvind Nilsson <>
2018-11-22 16:59:10 +0100

    Menu 22/11/18
    Creating menu.

--- src/stud/hkr/Main.java
+++ src/stud/hkr/Main.java
@@ -1,6 +1,11 @@
 package stud.hkr;
 
+
+import java.util.Scanner;
+
 public class Main {
+
+    private Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
 	    Main hotelApp = new Main();
@@ -10,10 +15,42 @@ public class Main {
     private void runProgram(){
         HotelLogic logic = new HotelLogic();
 
-        drawMenu();
+        boolean running = true;
+
+        while (running) {
+            drawMenu();
+            int menuChoice = input.nextInt();
+
+            switch (menuChoice){
+                case 1: {
+                    // make a costumer menu
+                    break;
+                }
+                case 2: {
+                    // make a room menu
+                    break;
+                }
+                case 3: {
+                    // make a booking menu
+                    break;
+                }
+                case 4: {
+                    running = false;
+                }
+            }
+        }
+
     }
 
     private void drawMenu(){
 
+        System.out.println("---------------------------------");
+        System.out.println("\tWelcome Valued Employee");
+        System.out.println("\nWhat will you be doing today?");
+        System.out.println("1) Customer");
+        System.out.println("2) Room");
+        System.out.println("3) Booking");
+        System.out.println("4) Nothing\n");
+        System.out.println("-----------------------------------");
     }
 }
