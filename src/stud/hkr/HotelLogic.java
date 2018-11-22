930c1ed852f205b2490bf0197f16f2c6b91fbdd1
petertenghamn <petertenghamn@hotmail.com>
2018-11-22 17:04:42 +0100

    expanded logic

--- src/stud/hkr/HotelLogic.java
+++ src/stud/hkr/HotelLogic.java
@@ -3,47 +3,70 @@ import java.util.ArrayList;
 
 public class HotelLogic {
 
-    Room[] rooms = new Room[100];
+    //should be array later
+    ArrayList<Room> rooms = new ArrayList<>();
     ArrayList<Customer> customers = new ArrayList<>();
 
-    public void getCustomer(String ssn){
+    public Customer getCustomer(String ssn){
+        //return a customer when asked for
         for (Customer c : customers){
-            if (c.getSsn() == ssn){
-                //return a customer? or error
+            if (c.getSsn().equals(ssn)){
+                return c;
             }
         }
+        return null;
     }
 
-    public void getCustomers(){
-        //get list of Customers
+    public ArrayList<Customer> getCustomers(){
+        //get list of all Customers
+        return customers;
     }
 
-    public void getRoom(int roomNbr){
+    public Room getRoom(int roomNbr){
         //get a Room
+        for (Room r : rooms){
+            if (r.getRoomNumber() == roomNbr){
+                return r;
+            }
+        }
+        return null;
     }
 
-    public void getRooms(){
+    public ArrayList<Room> getRooms(){
         //get list of Rooms
+        return rooms;
     }
 
-    public void getAvailableRooms(){
+    public ArrayList<Room> getAvailableRooms(){
         //get list of Rooms not booked
+        ArrayList<Room> output = new ArrayList<>();
+        for (Room r : rooms){
+            if (r.getIsBooked()){
+                output.add(r);
+            }
+        }
+        return output;
     }
 
-    public void addCustomer(Customer customer){
+    public void addCustomer(Customer c){
         //add a customer to all current customers
+        customers.add(c);
     }
 
-    public void addRoom(Room room){
+    public void addRoom(Room r){
         //add a room to all current rooms
+        rooms.add(r);
     }
 
     public void checkInCustomer(String ssn, int bookingnbr){
         //check in a customer to an available room
+        //get person, from person get booking, from booking get rooms to change to being booked = true
+
     }
 
     public void checkOutCustomer(String ssn, int bookingnbr){
         //check out a customer out of a room currently used
+
     }
 
     public void editBooking(String ssn, Booking booking){
