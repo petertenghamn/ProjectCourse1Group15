4aa99a571236cbf31bc71752d2e253fe3a33a571
Öjvind Nilsson <>
2018-11-22 14:46:49 +0100

    test Commit 22/11/18
    Created a getter and constructor method.

--- src/stud/hkr/Booking.java
+++ src/stud/hkr/Booking.java
@@ -1,5 +1,24 @@
 package stud.hkr;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Booking {
+
+    private ArrayList roomlist;
+    private int bookingId;
+    private Date checkInDate;
+    private Date checkOutDate;
+    private double totalPrice;
+
+    public Booking (ArrayList<Room> rooms, Date checkO, Date checkI){
+        roomlist = rooms;
+        checkInDate = checkI;
+        checkOutDate = checkO;
+}
+
+    public int getBookingId() {
+        return bookingId;
+    }
 
 }
