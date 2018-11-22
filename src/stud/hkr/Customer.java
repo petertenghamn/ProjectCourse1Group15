8d2006f57fe1be35f7821f1fc79b569a28f0978e
steswan <45011831+steswan@users.noreply.github.com>
2018-11-22 13:51:39 +0100

    Customer class commit

--- src/stud/hkr/Customer.java
+++ src/stud/hkr/Customer.java
@@ -2,4 +2,19 @@ package stud.hkr;
 
 public class Customer {
 
+    private String ssn;
+    private String name;
+    private String address;
+    private String telephoneNumber;
+
+    public Customer(String ssn, String name, String address, String telephoneNumber) {
+        this.ssn = ssn;
+        this.name = name;
+        this.address = address;
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
